de40fcbcc87f5f8bfa67dd8f9fa563d2f4c0916e
Ar Corsino <corsinoa@student.mville.edu>
2025-02-23 22:33:35 -0500

    second

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
-import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
+import DoctorHome from "./DoctorHome";
+import App from "./App"; // Other page/component, e.g., your Todos page
 import "./index.css";
-import '@aws-amplify/ui-react/styles.css';
+import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+// Configure Amplify with your backend outputs
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DoctorHome />} />
+          <Route path="/todos" element={<App />} />
+          {/* Add additional routes here if needed */}
+        </Routes>
+      </BrowserRouter>
     </Authenticator>
   </React.StrictMode>
 );
